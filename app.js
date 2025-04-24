f938ae6e62bfee2672d275d7391c7436bfbf6ad9
Tauf4430d <amee4430d@gmail.com>
2025-04-24 11:47:38 +0530

    Basic functionality is done

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const userForm = document.querySelector("#usernameF");
 const passwordForm = document.querySelector("#passwordF");
 
 
-//variables
-let editI = false;
-
+//start of the storage or existing values
+let storedPasswords;
+let passToEdit = null;
+let editV = false;
+const data = localStorage.getItem("passwords");
+storedPasswords = data ? JSON.parse(data) : [];
 
 //functions
 class pass {
@@ -23,32 +26,79 @@ class pass {
 }
 
 function copy(tar) {
-  console.log("Copy : ", tar);
+  //https://www.freecodecamp.org/news/copy-text-to-clipboard-javascript/
+
+  const toCopyOnClipBoard = tar.parentElement.parentElement.children[2].innerText;
+  navigator.clipboard.writeText(toCopyOnClipBoard)
+  .then(() => {
+    showToast("password copied");
+  })
+  .catch(() => {
+    showToast("didn't copied");
+  })
+}
+
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  toast.innerText = message;
+  toast.classList.add("show");
+
+  // Hide after 3 seconds
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000);
 }
 
 function show(tar) {
-  console.log("Show : ", tar);
+  const target = tar.closest(".pass").children[2].children[0];
+  const btn = tar.closest(".pass").children[3].children[0];
+  const isVisible = btn.getAttribute("data-visible");
+  if(isVisible === "true") {
+    btn.src = "assests/eye.png";
+    target.innerHTML = "********";
+    btn.setAttribute("data-visible", false);
+  } else {
+    btn.src = "assests/hide.png";
+    target.innerHTML = target.getAttribute("data-password");
+    setTimeout(() => {
+      btn.src = "assests/eye.png";
+      target.innerHTML = "********";
+      btn.setAttribute("data-visible", false);
+      return;
+    }, 5000);
+    btn.setAttribute("data-visible", true);
+  }
 }
 
 function del(tar) {
-  const toDelete = tar.parentElement.parentElement;
-  console.log(toDelete);
+  const toDelete = tar.closest(".pass");
+  storedPasswords = storedPasswords.filter(pass => parseInt(pass.id) !== parseInt(toDelete.id));
+  localStorage.setItem("passwords", JSON.stringify(storedPasswords));
   toDelete.remove();
 }
 function edit(tar) {
-  editI = true;
+  editV  = true
   const modal = document.querySelector(".popup-content");
-  const target = tar.parentElement.parentElement;
+  const target = tar.closest(".pass");
   const website = target.children[0].innerText;
   const username = target.children[1].innerText;
-  const password = target.children[2].innerText;
-  
+  const password = target.children[2].children[0].getAttribute("data-password");
+  passToEdit = parseInt(target.id);
   modal.children[1].value = website;
   modal.children[2].value = username;
   modal.children[3].value = password;
   document.querySelector(".popup").style.display = "flex";
 }
 
+function render() {
+  document.querySelector(".container").innerHTML = "";
+  if(storedPasswords.length > 0) {
+    const passwords = JSON.parse(localStorage.getItem("passwords"));
+    passwords.forEach((pass) => {
+      createPass(pass);
+  });
+}
+}
 function createPass(obj) {
   const container = document.querySelector(".container");
   const toInsert = `<div class="pass" id="${obj.id}">
@@ -60,22 +110,44 @@ function createPass(obj) {
           <p>${obj.username}</p>
         </div>
         <div class="password">
-          <p>${obj.password}</p>
+          <p class="password-text" data-password="${obj.password}">********</p>
         </div>
         <div class="options">
-          <img src="assests/eye.png" width="30px" id="show"/>
-          <img src="assests/copy.png" width="30px" id="copy"/>
-          <img src="assests/cross.png" width="30px" id="delete"/>
-          <img src="assests/pencil.png" width="30px" id="edit"/>
+          <img src="assests/eye.png" width="30px" class="show" data-visible="false"/>
+          <img src="assests/copy.png" width="30px" class="copy"/>
+          <img src="assests/cross.png" width="30px" class="delete"/>
+          <img src="assests/pencil.png" width="30px" class="edit"/>
         </div>
       </div>`
   container.insertAdjacentHTML("beforeend", toInsert);
 }
 
+
+
 //producing all ".pass" dynamically when the page loads using onload.
 window.addEventListener('load', () => {
-
+  render();
 })
+
+// document.addEventListener("click", function(e) {
+//   console.log("runs")
+//   if (e.target && e.target.id === "show") {
+//     const passContainer = e.target.closest(".pass");
+//     const passText = passContainer.querySelector(".password-text");
+
+//     const isHidden = passText.textContent.includes("•");
+
+//     if (isHidden) {
+//       // Show real password
+//       passText.textContent = passText.getAttribute("data-password");
+//       e.target.src = "assests/eye-off.png"; // Optional: change icon
+//     } else {
+//       // Hide it back
+//       passText.textContent = "********";
+//       e.target.src = "assests/eye.png"; // Back to original icon
+//     }
+//   }
+// });
 
 //adding a new password
 add.addEventListener('click', () => {
@@ -88,26 +160,32 @@ close.addEventListener('click', () => {
   webForm.value = '';
   userForm.value = '';
   passwordForm.value = '';
-  editI = false;
 });
 
 //submitting the values for new password.
-submit.addEventListener('click', () => {
+submit.addEventListener('click', (e) => {
   if(!webForm.value || !userForm.value || !passwordForm.value) {
     //throw an error on the #warnings paragraph
     document.querySelector("#warnings").innerHTML = `Enter Valid Credentials`;
     return;
   }
-  if(editI) {
-    
-    editI = false;
+  if(editV) {
+    const modal = document.querySelector(".popup-content");
+    storedPasswords = storedPasswords.filter((pass) => {
+      if(parseInt(pass.id) === passToEdit) {
+        pass.website = modal.children[1].value;
+        pass.username = modal.children[2].value;
+        pass.password = modal.children[3].value;
+      }
+      return pass;
+    });
+    editV = false;
+    passToEdit = null;
   } else {
-    arr.push(new pass(count, webForm.value, userForm.value, passwordForm.value));
-    createPass(arr[arr.length-1]);
-    count++;
-    console.log(count);
+    storedPasswords.push(new pass(Date.now(), webForm.value, userForm.value, passwordForm.value));
   }
-
+  localStorage.setItem("passwords", JSON.stringify(storedPasswords)); 
+  render();
   webForm.value = '';
   userForm.value = '';
   passwordForm.value = '';
@@ -119,7 +197,7 @@ submit.addEventListener('click', () => {
 //parent element of the ".pass" and so when more added it can be handled
 container.addEventListener('click', (e) => {
   if(e.target.tagName === 'IMG') {
-    switch (e.target.id) {
+    switch (e.target.className) {
       case "delete" :
         del(e.target);
         break;
